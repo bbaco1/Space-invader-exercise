43a4fa5b10bfdc1876dec9351f1d604f1b8f99d1
Milorad Orzes <milorad@honig.io>
2017-02-05 14:36:06 +0100

    Initial Commit

---
+++ Space Invaders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space Invaders
+//
+//  Created by Milorad Orzes on 05/02/2017.
+//  Copyright © 2017 Milorad Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
